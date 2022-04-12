78c294a2f52633f6cc72a410db607ce72c88ba3b
Alexander Schwarze <shikami97@gmx.de>
2022-04-12 17:23:01 +0200

    Project Setup part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,36 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
-group = "me.shika"
-version = "1.0-SNAPSHOT"
+group = "alex.brett"
+version = "1.0"
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "MemeScreen"
+            packageVersion = "1.0.0"
+        }
+    }
 }
---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,8 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+
+@Composable
+@Preview
+fun App() {
+
+}
