e254950631a67097fe69fe648c8e1cf507fcc11c
Alexander Schwarze <shikami97@gmx.de>
2022-04-13 17:05:20 +0200

    Small adjustments considering the timer

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,5 +1,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -8,6 +9,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -19,20 +21,24 @@ import java.util.*
 import kotlin.random.Random
 
 val APP_DATA: String = System.getenv("APPDATA")
+var rendering = false
 @Composable
 @Preview
 fun App() {
     val grid_x = remember{ mutableStateOf(TextFieldValue("3"))}
     val grid_y = remember{ mutableStateOf(TextFieldValue("2"))}
-    val startInterval = remember{ mutableStateOf(TextFieldValue("60"))}
-    val reductionAmount = remember{ mutableStateOf(TextFieldValue("5"))}
+    val startInterval = remember{ mutableStateOf(TextFieldValue("60000"))}
+    val reductionAmount = remember{ mutableStateOf(TextFieldValue("5000"))}
+    val inUsage = remember { mutableStateOf(false) }
     var currentTimer = "0"
+    var timer = Timer()
     MaterialTheme {
         Column (){
             Row {
                 OutlinedTextField(
                     value = grid_x.value,
                     onValueChange = { grid_x.value = it },
+                    readOnly = inUsage.value,
                     label = { Text(
                         text = "Amount Columns",
                         fontSize = 15.sp,
@@ -56,6 +62,7 @@ fun App() {
                 OutlinedTextField(
                     value = grid_y.value,
                     onValueChange = { grid_y.value = it },
+                    readOnly = inUsage.value,
                     label = { Text(
                         text = "Amount Lines",
                         fontSize = 15.sp,
@@ -80,8 +87,9 @@ fun App() {
                 OutlinedTextField(
                     value = startInterval.value,
                     onValueChange = { startInterval.value = it },
+                    readOnly = inUsage.value,
                     label = { Text(
-                        text = "Start Interval (in seconds)",
+                        text = "Start Interval (in milliseconds)",
                         fontSize = 15.sp,
                         color = Color.Black
                     ) },
@@ -104,8 +112,9 @@ fun App() {
                 OutlinedTextField(
                     value = reductionAmount.value,
                     onValueChange = { reductionAmount.value = it },
+                    readOnly = inUsage.value,
                     label = { Text(
-                        text = "Interval Reduction Amount (in seconds)",
+                        text = "Interval Reduction Amount (in milliseconds)",
                         fontSize = 15.sp,
                         color = Color.Black
                     ) },
@@ -128,14 +137,24 @@ fun App() {
                     .fillMaxWidth()
             ) {
                 Button(
+                    enabled = !inUsage.value,
                     onClick = {
                         val startTime = System.currentTimeMillis()
-                        Timer().schedule( object: TimerTask(){
+                        inUsage.value = true
+                        timer = Timer()
+                        timer.schedule( object: TimerTask(){
                             override fun run() {
-                                renderHTML(grid_x.value.text, grid_y.value.text, startInterval.value.text, reductionAmount.value.text)
+                                rendering = true
+                                renderHTML(
+                                    grid_x.value.text,
+                                    grid_y.value.text,
+                                    startInterval.value.text,
+                                    reductionAmount.value.text,
+                                    true
+                                )
                                 currentTimer = ((System.currentTimeMillis() - startTime) / 1000 ).toString()
                             }
-                        }, 0L, startInterval.value.text.toLong() * 1000)
+                        }, 0L, startInterval.value.text.toLong())
                     },
                     modifier = Modifier
                         .padding(bottom = 15.dp, start = 5.dp, top = 30.dp)
@@ -150,17 +169,58 @@ fun App() {
 
                 )
             }
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {
+                        inUsage.value = false
+                        renderHTML(
+                            grid_x.value.text,
+                            grid_y.value.text,
+                            startInterval.value.text,
+                            reductionAmount.value.text,
+                            false
+                        )
+                        timer.cancel()
+                    },
+                    modifier = Modifier
+                        .padding(bottom = 15.dp, start = 5.dp, top = 30.dp)
+                ) {
+                    Text(text = "Stop Rendering Loop")
+                }
+            }
         }
-    }
-    fun timerHandler(){
-
     }
 }
 
+var currentRenderIteration = 0
 var lastRandomIndex = -1
 
-fun renderHTML(grid_x: String, grid_y: String, startInterval: String, reductionAmount: String){
-    println("Start RENDER")
+override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+    return when (keyCode) {
+        KeyEvent.KEYCODE_D -> {
+            moveShip(MOVE_LEFT)
+            true
+        }
+        KeyEvent.KEYCODE_F -> {
+            moveShip(MOVE_RIGHT)
+            true
+        }
+        KeyEvent.KEYCODE_J -> {
+            fireMachineGun()
+            true
+        }
+        KeyEvent.KEYCODE_K -> {
+            fireMissile()
+            true
+        }
+        else -> super.onKeyUp(keyCode, event)
+    }
+}
+
+fun renderHTML(grid_x: String, grid_y: String, startInterval: String, reductionAmount: String, rendering: Boolean){
     val folder = "$APP_DATA/MemeScreen"
     val path = Path.of(folder)
     if(!Files.exists(path)){
@@ -173,25 +233,25 @@ fun renderHTML(grid_x: String, grid_y: String, startInterval: String, reductionA
         var gridString = "<div class=\"grid-container\">\n"
 
         val single = "<div></div>\n"
-        var i = 0
-        var currentRandomField = Random.nextInt(gridXNumber * gridYNumber)
-        println(lastRandomIndex)
+        var bound = gridXNumber * gridYNumber
+        if(bound <= 0){
+            bound = 1
+        }
+        var currentRandomField = Random.nextInt(bound)
+        //println(lastRandomIndex)
         while(currentRandomField == lastRandomIndex){
-            currentRandomField = Random.nextInt(gridXNumber * gridYNumber)
+            currentRandomField = Random.nextInt(bound)
         }
         lastRandomIndex = currentRandomField
-        while (i < gridYNumber){
-            var j = 0
-            while(j < gridXNumber) {
+        for(i in 0 until gridYNumber) {
+            for(j in 0 until gridXNumber) {
                 var tmp = single
                 val currentIndex = i * gridXNumber + j
                 if (currentIndex == currentRandomField) {
-                    tmp = tmp.replace("<div>", "<div style=\"background: black;\">")
+                    tmp = tmp.replace("<div>", "<div style=\"background: black;\"><span style = \"color: white;\">$currentRenderIteration</span>")
                 }
                 gridString += tmp
-                j++
             }
-            i++
         }
 
         gridString += "</div>"
@@ -202,17 +262,22 @@ fun renderHTML(grid_x: String, grid_y: String, startInterval: String, reductionA
         return "auto ".repeat(gridXNumber)
     }
 
-    val htmlCode = "" +
+    var htmlCode = "" +
             "<!DOCTYPE html>\n" +
             "<html>\n" +
-                "<head>\n" +
-                "<meta http-equiv=\"refresh\" content=\"1\">\n" +
-                "<style>\n" +
+                "<head>\n"
+    if(rendering) {
+        htmlCode += "<meta http-equiv=\"refresh\" content=\"0.1\">\n"
+    }
+    htmlCode += "<style>\n" +
                     ".grid-container{\n" +
                         "display: grid;\n" +
                         "grid-template-columns: " + getColumnString() + ";\n" +
                         "width: 100vw;\n" +
                         "height: 100vh;\n" +
+                    "}\n" +
+                    "html{\n" +
+                        "overflow: hidden;\n" +
                     "}\n" +
                 "</style>\n" +
                 "</head>\n" +
@@ -224,6 +289,7 @@ fun renderHTML(grid_x: String, grid_y: String, startInterval: String, reductionA
     File(APP_DATA + "/MemeScreen/MemeScreen.html").printWriter().use { out ->
         out.println(htmlCode)
     }
-    println("Generated HTML code: \n$htmlCode")
-    println("End RENDER")
+    // println("Generated HTML code: \n$htmlCode")
+    currentRenderIteration++
+    println("Finished Rendering $currentRenderIteration")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,24 @@
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
-
+import java.util.*
+import kotlin.system.exitProcess
 
 fun main() = application {
 
     Window(
-        onCloseRequest = ::exitApplication,
+        onCloseRequest = {
+            Timer().cancel()
+            renderHTML("0", "0", "1", "1", false)
+            exitProcess(0)
+        },
         title = "MemeScreen",
         state = WindowState(
             position = WindowPosition(Alignment.Center),
             size = WindowSize(400.dp, 700.dp)
         ),
         resizable = false
-    ){
+    ) {
         App()
     }
 }
